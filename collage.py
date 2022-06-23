25e1f745b3ea3115163e6999aa52b150bd954171
algo <103216698+algohome@users.noreply.github.com>
2022-06-23 17:23:25 -0400

    Created collage.py
    Initial commit for main script

---
+++ collage.py
@@ -0,0 +1,7 @@
+from AutoCollager import AutoCollager
+
+ac = AutoCollager('/PATH/TO/TARGET/IMAGE',
+                '/PATH/TO/DIRECTORY/OF/IMAGES',
+                '/PATH/FOR/OUTPUT/IMAGES',
+                 n_samples = 128,
+                 square_size=32)
